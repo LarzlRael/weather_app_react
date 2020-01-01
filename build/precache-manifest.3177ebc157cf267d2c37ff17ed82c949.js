d2a96c42fd2fb31ef11b7d05d9802449edd88911
larz <rey_gato10@hotmail.com>
2020-01-01 12:21:28 -0400

    este tiene la carpeta de prod

---
+++ build/precache-manifest.3177ebc157cf267d2c37ff17ed82c949.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21397af9b299456fd3540f2ba35423c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "affff6a6026a6a1243f3",
+    "url": "/static/css/2.22d6b5ec.chunk.css"
+  },
+  {
+    "revision": "d0a2c41c77af30a80116",
+    "url": "/static/css/main.953ff46a.chunk.css"
+  },
+  {
+    "revision": "affff6a6026a6a1243f3",
+    "url": "/static/js/2.46c4702c.chunk.js"
+  },
+  {
+    "revision": "04b21ccc9109def6e71671ed6f2d794a",
+    "url": "/static/js/2.46c4702c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d0a2c41c77af30a80116",
+    "url": "/static/js/main.9e5d26a1.chunk.js"
+  },
+  {
+    "revision": "e67f24e5d53adad52d5b",
+    "url": "/static/js/runtime-main.f7a665f6.js"
+  }
+]);
